f7e519f109708bed662b768f49b5bff17195e4ce
aparjun <46344050+aparjun@users.noreply.github.com>
2019-06-01 11:47:18 +0200

    Update Roman To Decimal.py

--- Roman To Decimal.py
+++ Roman To Decimal.py
@@ -1,10 +1,16 @@
 n=input()
 d=0
+i=0
 li={"H":10000,"G":5000,"M":1000,"D":500,"C":100,"L":50,"X":10,"V":5,"I":1}
-for i in range(0,len(n)-1):
-	if(li[n[i]]<li[n[i+1]]):
-		d=d+li[n[i+1]]-li[n[i]]
+while(i<len(n)):
+	if(i+1<len(n)):
+		if(li[n[i]]<li[n[i+1]]):
+			d=d+li[n[i+1]]-li[n[i]]
+			i+=2
+		else:
+			d+=li[n[i]]
+			i+=1
 	else:
 		d+=li[n[i]]
-d+=li[n[len(n)-1]]
+		i+=1
 print(d)
