40ab0cdeb81b07934e79c675dd8eefd9480124ea
aparjun <46344050+aparjun@users.noreply.github.com>
2019-06-07 13:43:27 +0200

    Update Number In Words.py

--- Number In Words.py
+++ Number In Words.py
@@ -4,7 +4,7 @@ def integer_to_english(number):
     s=""
     o={0:"zero",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
     teen={11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
-    t={0:"",1:"ten",2:"twenty",3:"thirty",4:"forty",5:"fifty",6:"sixty",7:"seventy",8:"eighty",9:"ninty"}
+    t={0:"",1:"ten",2:"twenty",3:"thirty",4:"forty",5:"fifty",6:"sixty",7:"seventy",8:"eighty",9:"ninety"}
     h={0:"",1:"one hundred",2:"two hundred",3:"three hundred",4:"four hundred",5:"five hundred",6:"six hundred",7:"seven hundred",8:"eight hundred",9:"nine hundred"}
     if(int(number)<1 or int(number)>1000):
         return -1
